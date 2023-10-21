795b53a3cd6f1e1fffde65e576563f7da91b6bc2
david.tarvin <david.tarvin@physiciansmutual.com>
2023-10-21 13:21:46 -0500

    add change and clear buttons and non-json string to change to json

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 const inputArea = document.querySelector(".large-area--input");
 const outputArea = document.querySelector(".large-area--output");
+const btnChange = document.querySelector(".controls__button--change");
 const btnFormat = document.querySelector(".controls__button--format");
 const btnMinify = document.querySelector(".controls__button--minify");
+const btnClear = document.querySelector(".controls__button--clear");
+
+function addQuotesToSubstring(substring) {
+    return '\" + substring + \"';
+}
+
+btnChange.addEventListener("click", () => {
+    let valueToChange = "[" + inputArea.value + "]";
+    var newValue = valueToChange.replaceAll("=", ":");
+
+    inputArea.value = newValue;
+});
 
 btnFormat.addEventListener("click", () => {
     const formatted = JSON.stringify(JSON.parse(inputArea.value), null, 4);
@@ -13,4 +26,11 @@ btnMinify.addEventListener("click", () => {
     const minified = JSON.stringify(JSON.parse(inputArea.value));
 
     outputArea.value = minified;
+});
+
+btnClear.addEventListener("click", () => {
+    const clear = "";
+
+    inputArea.value = clear;
+    outputArea.value = clear;
 });
